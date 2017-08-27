839e6eac1c742d49651373e816199cab733d043e
Niels Koster <n.koster@portavita.eu>
2017-08-27 16:03:17 +0200

    Eyes.

--- get.js
+++ get.js
@@ -2,17 +2,17 @@
 
 let hostFeed = '';
 if (process.argv.indexOf("-host") !== -1) {
-    hostFeed = process.argv[process.argv.indexOf("-host") + 1];
+    hostFeed = process.argv[process.argv.indexOf("-host") + 1]
 }
 
 let hostPort = '';
 if (process.argv.indexOf("-port") !== -1) {
-    hostPort = process.argv[process.argv.indexOf("-port") + 1];
+    hostPort = process.argv[process.argv.indexOf("-port") + 1]
 }
 
 let hostPath = '';
 if (process.argv.indexOf("-path") !== -1) {
-    hostPath = process.argv[process.argv.indexOf("-path") + 1];
+    hostPath = process.argv[process.argv.indexOf("-path") + 1]
 }
 
 const
@@ -35,7 +35,7 @@ function getItems(content, tag) {
         if (tagEnd > tagStart) {
             s = content.substr(tagStart, tagEnd - tagStart + tag.length + 2);
             arr.push(s);
-            content = content.replace(s, '');
+            content = content.replace(s, '')
         }
     }
     return arr
@@ -58,10 +58,10 @@ const parser = function(res) {
 
 if (hostPort === '80') {
     http.get(options, parser).on('error', e => {
-        console.error(e.message);
+        console.error(e.message)
     });
 } else {
     https.get(options, parser).on('error', e => {
-        console.error(e.message);
+        console.error(e.message)
     });
 }
