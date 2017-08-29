492aa6d9943004b29c66231491122c6a9ba08f98
Niels Koster <n.koster@portavita.eu>
2017-08-30 00:35:44 +0200

    Convert to npm module.

--- get.js
+++ get.js
@@ -1,107 +1,93 @@
 'use strict';
 
-let hostFeed = '', hostPort = '80', hostPath = '', hostURL = '';
+exports.getRssAtom = function(feedUrl) {
 
-if (process.argv.indexOf("-host") !== -1) {
-    hostFeed = process.argv[process.argv.indexOf("-host") + 1]
-}
+    const
+        url = require('url'),
+        feed = url.parse(feedUrl),
+        http = require('http'),
+        https = require('https'),
+        options = {
+            host: feed.hostname,
+            port: feed.port,
+            path: feed.path
+        };
 
-if (process.argv.indexOf("-port") !== -1) {
-    hostPort = process.argv[process.argv.indexOf("-port") + 1]
-}
+    function getItem(content, tag) {
+        let tagStart = 0, tagEnd = 0, s = '', count = 0;
+        while (content.search('<' + tag) > 0 && count < content.length) {
+            tagStart = content.search('<' + tag);
+            tagEnd = content.search('/' + tag + '>');
+            if (tagEnd > tagStart) {
+                s = content.substr(tagStart, tagEnd - tagStart + tag.length + 2);
+                content = content.replace(s, '')
+            } else {
+                count++
+            }
+        }
+        return s.replace("<![CDATA[", "")
+            .replace("]]>", "")
+            .replace(/<[^>]+>/g, '')
+            .replace(/\r?\n|\r/g, '')
+    }
 
-if (process.argv.indexOf("-path") !== -1) {
-    hostPath = process.argv[process.argv.indexOf("-path") + 1]
-}
-
-if (process.argv.indexOf("-url") !== -1) {
-    hostURL = process.argv[process.argv.indexOf("-url") + 1]
-}
-
-const
-    url = require('url'),
-    feed = url.parse(hostURL),
-    http = require('http'),
-    https = require('https'),
-    options = {
-        host: feed.hostname,
-        port: feed.port,
-        path: feed.path
-    };
-
-function getItem(content, tag) {
-    let tagStart = 0, tagEnd = 0, s = '', count = 0;
-    while (content.search('<' + tag) > 0 && count < content.length) {
-        tagStart = content.search('<' + tag);
-        tagEnd = content.search('/' + tag + '>');
-        if (tagEnd > tagStart) {
-            s = content.substr(tagStart, tagEnd - tagStart + tag.length + 2);
-            content = content.replace(s, '')
+    function getItemAtom(content, tag) {
+        const match = content.match('<' + tag + '(.*)?/>');
+        if (match !== null) {
+            return match[0].match(/"(.*)"/)[0].replace(/"/g, '')
         } else {
+            return ''
+        }
+    }
+
+    function getItems(content, tag) {
+        let arr = [], tagStart = 0, tagEnd = 0, count = 0;
+        while (content.search('<' + tag) > 0 && count < content.length) {
+            tagStart = content.search('<' + tag);
+            tagEnd = content.search('/' + tag + '>');
+            if (tagEnd > tagStart) {
+                let
+                    s = content.substr(tagStart, tagEnd - tagStart + tag.length + 2),
+                    t = getItem(s, 'title'),
+                    c = getItem(s, 'content').concat(getItem(s, 'description')),
+                    l = getItem(s, 'link');
+                if (t === '') t = '[Failed]';
+                if (c === '') c = '[Failed]';
+                if (l === '') l = getItemAtom(s, 'link');
+                if (l === '') l = '[Failed]';
+                arr.push({title: t, description: c, href: l});
+                content = content.replace(s, '')
+            }
             count++
         }
+        return arr
     }
-    return s.replace("<![CDATA[", "")
-        .replace("]]>", "")
-        .replace(/<[^>]+>/g, '')
-        .replace(/\r?\n|\r/g, '')
-}
 
-function getItemAtom(content, tag) {
-    const match = content.match('<' + tag + '(.*)?/>');
-    if (match !== null) {
-        return match[0].match(/"(.*)"/)[0].replace(/"/g, '')
-    } else {
-        return ''
+    function extractItems(content) {
+        return getItems(content.toString(), 'item')
+            .concat(getItems(content.toString(), 'entry'))
     }
-}
 
-function getItems(content, tag) {
-    let arr = [], tagStart = 0, tagEnd = 0, count = 0;
-    while (content.search('<' + tag) > 0 && count < content.length) {
-        tagStart = content.search('<' + tag);
-        tagEnd = content.search('/' + tag + '>');
-        if (tagEnd > tagStart) {
-            let
-                s = content.substr(tagStart, tagEnd - tagStart + tag.length + 2),
-                t = getItem(s, 'title'),
-                c = getItem(s, 'content').concat(getItem(s, 'description')),
-                l = getItem(s, 'link');
-            if (t === '') t = '[Failed]';
-            if (c === '') c = '[Failed]';
-            if (l === '') l = getItemAtom(s, 'link');
-            if (l === '') l = '[Failed]';
-            arr.push( { title: t, description: c, href: l } );
-            content = content.replace(s, '')
-        }
-        count++
-    }
-    return arr
-}
+    const parser = function (res) {
+        let content = '', count = 0;
+        res.on('data', chunk => content += chunk);
+        res.on('end', () => {
+            extractItems(content).forEach((item) => {
+                count++;
+                console.log(`\n--${count}-- ${item.title}\n${item.description}\n${item.href}\n`)
+            })
+        });
+        console.log(`Got response: ${res.statusCode}`)
+    };
 
-function extractItems(content) {
-    return getItems(content.toString(), 'item')
-        .concat(getItems(content.toString(), 'entry'))
-}
-
-const parser = function(res) {
-    let content = '', count = 0;
-    res.on('data', chunk => content += chunk);
-    res.on('end', () => {
-        extractItems(content).forEach((item) => {
-            count++;
-            console.log(`\n--${count}-- ${item.title}\n${item.description}\n${item.href}\n`)
+    if (feed.port === '80') {
+        http.get(options, parser).on('error', e => {
+            console.error(e.message)
         })
-    });
-    console.log(`Got response: ${res.statusCode}`)
-};
+    } else {
+        https.get(options, parser).on('error', e => {
+            console.error(e.message)
+        })
+    }
 
-if (hostPort === '80') {
-    http.get(options, parser).on('error', e => {
-        console.error(e.message)
-    })
-} else {
-    https.get(options, parser).on('error', e => {
-        console.error(e.message)
-    })
-}
+};
