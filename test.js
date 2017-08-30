c0ad99e7bd947302e48b224488baa5f08cc347c5
Niels Koster <n.koster@portavita.eu>
2017-08-30 18:02:17 +0200

    Initial.

---
+++ test.js
@@ -0,0 +1,14 @@
+test = require('./get');
+
+let
+    count = 0;
+    hostFeed = '';
+
+if (process.argv.length > 1) {
+    hostFeed = process.argv[2]
+}
+
+test.getRssAtom(hostFeed, content => content.forEach((item) => {
+    count++;
+    console.log(`\n--${count}-- ${item.title}\n${item.description}\n${item.href}\n`)
+}));
