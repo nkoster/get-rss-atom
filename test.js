1b971dcf274c9e32349eba9e80639b1541a2197d
Niels Koster <n.koster@portavita.eu>
2017-08-30 21:09:15 +0200

    Better name.

--- test.js
+++ test.js
@@ -2,13 +2,13 @@ test = require('get-rss-son/get');
 
 let
     count = 0;
-    hostFeed = '';
+    feedUrl = '';
 
 if (process.argv.length > 1) {
-    hostFeed = process.argv[2]
+    feedUrl = process.argv[2]
 }
 
-test.getRssAtom(hostFeed, content => content.forEach((item) => {
+test.getRssAtom(feedUrl, content => content.forEach((item) => {
     count++;
     console.log(`\n--${count}-- ${item.title}\n${item.description}\n${item.href}\n`)
 }));
